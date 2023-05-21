0b4d6e51f0358708d939a005bf4d5bcbd30cc1e0
Grant Me <210210502010@student.unm.ac.id>
2023-05-21 21:53:14 +0800

    Rev Profil

---
+++ app/src/main/java/com/example/grantme2/penyedia_UbahBeasiswa.java
@@ -0,0 +1,14 @@
+package com.example.grantme2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class penyedia_UbahBeasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penyedia_ubah_beasiswa);
+    }
+}
